7d74982e40b53052dd9083d4908c76f28d52f568
gmvivekanandan <joker12345.rm@gmail.com>
2021-06-24 18:50:19 +0530

    added responsive side bar

--- pages/[detail].js
+++ pages/[detail].js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Detail() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-auto sm:h-screen flex justify-center items-center">
       <div className="flex flex-col rounded-md sm:flex-row">
         <Image
           src="/images.jpeg"
@@ -55,7 +55,7 @@ export default function Detail() {
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
             </svg>
           </div>
-          <p className="w-96 flex-grow">
+          <p className="w-auto sm:w-96 overflow-auto flex-grow">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
