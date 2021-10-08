e5487b710b7d48f2cb6ca75a862577d3c5042903
gmvivekanandan <joker12345.rm@gmail.com>
2021-10-08 16:26:17 +0530

    finished [detail].js

--- components/card.js
+++ components/card.js
@@ -1,25 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Card(props) {
   const router = useRouter();
   return (
-    <div
-      className="flex-row flex sm:flex-col m-3 shadow-md bg-ivory-light border-ivory-light rounded-md hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 select-none cursor-pointer"
-      onClick={() => router.push(props.id)}
+    <Link
+      href={{
+        pathname: "/[detail]",
+        query: { detail: props.id },
+      }}
     >
-      <Image
-        src={props.url}
-        alt="tee"
-        width={175}
-        height={250}
-        className="rounded-l-md sm:rounded-t-md sm:rounded-l-none"
-      />
-      <div className="flex flex-col">
-        <h2 className="text-xl font-bold ml-2 mt-2 p-2">{props.name}</h2>
-        <h2 className="text-sm font-light ml-2 p-2">A simple tagline</h2>
-        <h1 className="text-2xl font-bold ml-2 p-2">$ {props.price}</h1>
+      <div
+        className="flex-row flex sm:flex-col m-3 shadow-md bg-ivory-light border-ivory-light rounded-md hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 select-none cursor-pointer"
+        /*onClick={() =>
+          router.push({
+            pathname: "[detail]",
+            query: { detail: props.id, id: props.id },
+          })
+        }*/
+      >
+        <Image
+          src={props.url}
+          alt="tee"
+          width={175}
+          height={250}
+          className="rounded-l-md sm:rounded-t-md sm:rounded-l-none"
+        />
+        <div className="flex flex-col">
+          <h2 className="text-xl font-bold ml-2 mt-2 p-2">{props.name}</h2>
+          <h2 className="text-sm font-light ml-2 p-2">A simple tagline</h2>
+          <h1 className="text-2xl font-bold ml-2 p-2">$ {props.price}</h1>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- pages/[detail].js
+++ pages/[detail].js
@@ -1,24 +1,25 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import firebase from "../config/firebase";
 
-export default function Detail() {
+export default function Detail(post) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="h-auto sm:h-screen flex justify-center items-center bg-ivory-dark">
       <div className="flex flex-col rounded-md sm:flex-row bg-ivory-light">
         <Image
-          src="/images.jpeg"
+          src={post.imageurl}
           alt="tee"
           width={275}
           height={400}
           className="rounded-l-md"
         />
         <div className="flex flex-col p-2 space-y-2">
-          <h1 className="text-xl text-gray-800 font-bold">Name</h1>
-          <p className="w-auto sm:w-96 overflow-auto flex-grow">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
+          <h1 className="text-xl text-gray-800 font-bold">{post.name}</h1>
+          <p className="w-auto sm:w-96 overflow-auto flex-grow">{post.price}</p>
           <div className="p-5 flex justify-center">
             <button
               className="bg-dusty p-2 w-full rounded-full text-black"
@@ -31,4 +32,23 @@ export default function Detail() {
       </div>
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await firebase.firestore().collection("all").get();
+  const posts = await res.docs;
+  const paths = posts.map((post) => ({
+    params: { detail: post.id },
+  }));
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await firebase
+    .firestore()
+    .collection("all")
+    .doc(params.detail)
+    .get();
+  const post = res.data();
+  return { props: post, revalidate: 10 };
 }
